08a3590e040081b50f434c51c43fab4bc64ad1b0
Rabii Bouhestine <rabiibouhestine@outlook.com>
2020-08-05 13:22:55 +0100

    updated projects page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,13 +3,13 @@ var store = [{
         "excerpt":"I used to have a blog called datavisualised.com. I had many posts in there and had a lot of fun making them.  I stopped posting there because.. life.. and eventually deleted the blog. I wish I didn’t now. I wish I had more posts in here.  ","categories": [],
         "tags": [],
         "url": "https://rabiibouhestine.github.io//I-wish-I-had-more-posts/",
-        "teaser": null
+        "teaser": "https://rabiibouhestine.github.io//assets/images/blogging.jpg"
       },{
-        "title": "Corona Virus Disease (COVID-19) Dashboard",
-        "excerpt":"               Comments                ","categories": [],
+        "title": "Twitter Analysis (NLP Web Application)",
+        "excerpt":"               Comments               ","categories": [],
         "tags": [],
-        "url": "https://rabiibouhestine.github.io//covid19",
-        "teaser": "https://rabiibouhestine.github.io//assets/images/flu.jpg"
+        "url": "https://rabiibouhestine.github.io//twitterApp",
+        "teaser": "https://rabiibouhestine.github.io//assets/images/twitter.jpg"
       },{
         "title": "World Happiness Report (2020) Dashboard",
         "excerpt":"               Comments               ","categories": [],
@@ -17,9 +17,9 @@ var store = [{
         "url": "https://rabiibouhestine.github.io//happiness",
         "teaser": "https://rabiibouhestine.github.io//assets/images/happy.jpg"
       },{
-        "title": "Twitter Analysis (NLP Web Application)",
-        "excerpt":"               Comments               ","categories": [],
+        "title": "Corona Virus Disease (COVID-19) Dashboard",
+        "excerpt":"               Comments                ","categories": [],
         "tags": [],
-        "url": "https://rabiibouhestine.github.io//twitterApp",
-        "teaser": "https://rabiibouhestine.github.io//assets/images/twitter.jpg"
+        "url": "https://rabiibouhestine.github.io//covid19",
+        "teaser": "https://rabiibouhestine.github.io//assets/images/flu.jpg"
       }]
