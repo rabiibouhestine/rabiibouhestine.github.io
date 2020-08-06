7b00ab51933e7ffc4ea0e5998eb545b3fd3d1a10
Rabii Bouhestine <rabiibouhestine@outlook.com>
2020-08-06 16:30:11 +0100

    added ufo post and edited about page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,9 +1,9 @@
 var store = [{
-        "title": "I wish I had more posts in here!",
-        "excerpt":"I used to have a blog called datavisualised.com. I had many posts in there and had a lot of fun making them.  I stopped posting there because.. life.. and eventually deleted the blog. I wish I didn’t now. I wish I had more posts in here.  ","categories": [],
+        "title": "Scraping UFO sightings data with R",
+        "excerpt":"The National UFO Reporting Center (NUFORC) is dedicated to the collection and dissemination of objective UFO Data. It has a rich archive of reported UFO sightings going back to the 14th century, but this data is provided as tables in web pages, which is not downloadable. The NUFORC ‘Online Database’...","categories": [],
         "tags": [],
-        "url": "https://rabiibouhestine.github.io//I-wish-I-had-more-posts/",
-        "teaser": "https://rabiibouhestine.github.io//assets/images/blogging.jpg"
+        "url": "https://rabiibouhestine.github.io//ufo_sightings/",
+        "teaser": "https://rabiibouhestine.github.io//assets/images/ufo/ufo.jpg"
       },{
         "title": "Twitter Analysis (NLP Web Application)",
         "excerpt":"               Comments               ","categories": [],
